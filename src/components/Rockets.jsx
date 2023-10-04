84ef56ed531949df77a09ee3c1e54993571aa038
mryeminthu <dhammaarchive@hotmail.com>
2023-10-04 09:34:37 -0700

    Implement rocket booking

--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -1,38 +1,71 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { reserveRocket, cancelReservation, fetchRockets } from '../redux/rocketActions';
 import '../rockets.css';
 
-const rocketsData = [
-  {
-    id: 1,
-    name: 'Falcon 1',
-    description:
-      'The Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009. On 28 September 2008, Falcon 1 became the first privately-developed liquid-fuel launch vehicle to go into orbit around the Earth.',
-  },
-  {
-    id: 2,
-    name: 'Falcon 2',
-    description:
-      'The Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009. On 28 September 2008, Falcon 1 became the first privately-developed liquid-fuel launch vehicle to go into orbit around the Earth.',
-  },
-];
+const Rockets = () => {
+  const dispatch = useDispatch();
+  const rocketData = useSelector((state) => state.rockets);
 
-const Rockets = () => (
-  <div className="roc-container">
-    {rocketsData.map((rocket) => (
-      <div key={rocket.id} className="roc-card">
-        <img
-          src={`rocket-image-url-${rocket.id}.jpg`}
-          alt={rocket.name}
-          className="roc-image"
-        />
-        <div className="roc-info">
-          <h2>{rocket.name}</h2>
-          <p>{rocket.description}</p>
-          <button type="button" className="reserve-button">Reserve Rocket</button>
-        </div>
-      </div>
-    ))}
-  </div>
-);
+  useEffect(() => {
+    fetch('https://api.spacexdata.com/v3/rockets')
+      .then((response) => response.json())
+      .then((data) => {
+        dispatch(fetchRockets(data));
+      })
+      .catch(() => {});
+  }, [dispatch]);
+
+  const handleReserve = (rocketId) => {
+    dispatch(reserveRocket(rocketId));
+  };
+
+  const handleCancelReservation = (rocketId) => {
+    dispatch(cancelReservation(rocketId));
+  };
+
+  return (
+    <div className="roc-container">
+      {rocketData.length > 0
+        && rocketData.map((rocket) => (
+          <div key={rocket.id} className={`roc-card ${rocket.reserved ? 'reserved' : ''}`}>
+            <div className="roc-image-container">
+              <img
+                src={rocket.flickr_images[0]}
+                alt={rocket.name}
+                className="roc-image"
+              />
+            </div>
+            <div className="roc-info">
+              <div className="roc-title">
+                <h2>{rocket.rocket_name}</h2>
+              </div>
+              <p>
+                {rocket.reserved && <span className="reserved-badge">Reserved</span>}
+                {rocket.description}
+              </p>
+              {rocket.reserved ? (
+                <button
+                  type="button"
+                  className="cancel-reservation-button"
+                  onClick={() => handleCancelReservation(rocket.id)}
+                >
+                  Cancel Reservation
+                </button>
+              ) : (
+                <button
+                  type="button"
+                  className="reserve-button"
+                  onClick={() => handleReserve(rocket.id)}
+                >
+                  Reserve Rocket
+                </button>
+              )}
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+};
 
 export default Rockets;
