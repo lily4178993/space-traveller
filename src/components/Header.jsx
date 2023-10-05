22fc41a6608ffdc11b3fe592087b743c9b660606
mryeminthu <dhammaarchive@hotmail.com>
2023-10-04 18:36:30 -0700

    Add CSS to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import logo from '../assets/planet.png';
 
-/**
- * Represents the header component of the website.
- * @returns {JSX.Element} The JSX representation of the Header component.
- */
 const Header = () => (
   <header>
-    <img src={logo} alt="logo" />
+    <div className="logo-container">
+      <img src={logo} alt="logo" />
+      <h1>Space Travelers&apos; Hub</h1>
+    </div>
     <nav>
       <ul>
-        <li><NavLink to="/missions" activeClassName="active">Missions</NavLink></li>
-        <li><NavLink to="/rockets" activeClassName="active">Rockets</NavLink></li>
+        <li><Link to="/rockets">Rockets</Link></li>
+        <li><Link to="/missions">Missions</Link></li>
+        <li><Link to="/profile">My Profile</Link></li>
       </ul>
     </nav>
   </header>
